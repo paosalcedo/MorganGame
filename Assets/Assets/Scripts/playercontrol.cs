f8ca50b5e3fc673d804d6494cee2ebf24ce7de5b
Unknown <paosalcedo@gmail.com>
2016-11-26 18:54:13 -0500

    Design/Visuals
    Trails now match player color

--- Assets/Assets/Scripts/playercontrol.cs
+++ Assets/Assets/Scripts/playercontrol.cs
@@ -12,25 +12,59 @@ public class playercontrol : MonoBehaviour {
     private SpriteRenderer sr;
 
     // Use this for initialization
-    void Start () {
 
+
+	void Start () {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+		{
 
         var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
         transform.position += move * moveSpeed * Time.deltaTime;
 
-            scoreText.text = score.ToString();
-        }
+        scoreText.text = score.ToString();
+
+		Color currentcolor = GetComponent<SpriteRenderer>().color;
+
+		//CHANGES TRAIL COLOR TO MATCH PLAYER COLOR aka currentcolor
+		if (currentcolor == colorlist [0]) 
+		{
+			transform.Find ("PinkTrail").GetComponent<TrailRenderer> ().enabled = false;
+			transform.Find ("BlueTrail").GetComponent<TrailRenderer> ().enabled = false;
+			transform.Find ("GreenTrail").GetComponent<TrailRenderer> ().enabled = false;
+			transform.Find ("OrangeTrail").GetComponent<TrailRenderer> ().enabled = true;
+			Debug.Log ("Trail is now Orange!");
+		}
+
+		else if (currentcolor == colorlist [1]) {
+			transform.Find ("PinkTrail").GetComponent<TrailRenderer> ().enabled = false;
+			transform.Find ("BlueTrail").GetComponent<TrailRenderer> ().enabled = false;
+			transform.Find ("OrangeTrail").GetComponent<TrailRenderer> ().enabled = false;
+			transform.Find ("GreenTrail").GetComponent<TrailRenderer> ().enabled = true;
+			Debug.Log ("Trail is now Green!");
+		} else if (currentcolor == colorlist [2]) {
+			transform.Find ("BlueTrail").GetComponent<TrailRenderer> ().enabled = false;
+			transform.Find ("OrangeTrail").GetComponent<TrailRenderer> ().enabled = false;
+			transform.Find ("GreenTrail").GetComponent<TrailRenderer> ().enabled = false;
+			transform.Find ("PinkTrail").GetComponent<TrailRenderer> ().enabled = true;
+			Debug.Log ("Trail is now Pink!");
+		} else if (currentcolor == colorlist [3]) {
+			transform.Find ("OrangeTrail").GetComponent<TrailRenderer> ().enabled = false;
+			transform.Find ("GreenTrail").GetComponent<TrailRenderer> ().enabled = false;
+			transform.Find ("PinkTrail").GetComponent<TrailRenderer> ().enabled = false;
+			transform.Find ("BlueTrail").GetComponent<TrailRenderer> ().enabled = true;
+			Debug.Log ("Trail is now Blue!");
+    	}
+		
+		}
 
     void OnCollisionEnter2D(Collision2D col)
     {
         Debug.Log("collision");
-
         Color currentcolor = GetComponent<SpriteRenderer>().color;
         Debug.Log("now: " + currentcolor);
 
@@ -47,45 +81,44 @@ public class playercontrol : MonoBehaviour {
             score++;
             Debug.Log("score: " + score);
             GetComponent<SpriteRenderer>().color = colorlist[Random.Range(0, 3)];
-        }
+		
+       		//green orb
+		}
+        	else if (currentcolor == colorlist[1] && col.gameObject.name.Contains("greenplatform"))  //if orb is green and hits green platform
+	        {
+	            Debug.Log("green on green");
+	            score++;
+	            Debug.Log("score: " + score);
+	            GetComponent<SpriteRenderer>().color = colorlist[Random.Range(0, 3)];
+	        }
 
-   
-        //green orb
+	        //orange orb
 
-        else if (currentcolor == colorlist[1] && col.gameObject.name.Contains("greenplatform"))  //if orb is green and hits green platform
-        {
-            Debug.Log("green on green");
-            score++;
-            Debug.Log("score: " + score);
-            GetComponent<SpriteRenderer>().color = colorlist[Random.Range(0, 3)];
-        }
+	        else if (currentcolor == colorlist[2] && col.gameObject.name.Contains("pinkplatform"))  //if orb is orange and hits orange platform //pink should work now
+	        {
+	            Debug.Log("orange on orange");
+	            score++;
+	            Debug.Log("score: " + score);
+	            GetComponent<SpriteRenderer>().color = colorlist[Random.Range(0, 3)];
+	        }
 
-        //orange orb
+	        //blue orb
 
-        else if (currentcolor == colorlist[2] && col.gameObject.name.Contains("pinkplatform"))  //if orb is orange and hits orange platform //pink should work now
-        {
-            Debug.Log("orange on orange");
-            score++;
-            Debug.Log("score: " + score);
-            GetComponent<SpriteRenderer>().color = colorlist[Random.Range(0, 3)];
-        }
+	        else if (currentcolor == colorlist[3] && col.gameObject.name.Contains("blueplatform"))  //if orb is blue and hits blue platform 
+	        {
+	            Debug.Log("blue on blue");
+	            score++;
+	            Debug.Log("score: " + score);
+	            GetComponent<SpriteRenderer>().color = colorlist[Random.Range(0, 3)];
+	        }
 
-        //blue orb
+	        else
+	        {
+	            Debug.Log("blue on orange");
+	            score -= 1;
+	            Debug.Log("score: " + score);
+	        }
+			
+	}
 
-        else if (currentcolor == colorlist[3] && col.gameObject.name.Contains("blueplatform"))  //if orb is blue and hits blue platform 
-        {
-            Debug.Log("blue on blue");
-            score++;
-            Debug.Log("score: " + score);
-            GetComponent<SpriteRenderer>().color = colorlist[Random.Range(0, 3)];
-        }
-
-        else
-        {
-            Debug.Log("blue on orange");
-            score -= 1;
-            Debug.Log("score: " + score);
-        }
-
-    }
 }
