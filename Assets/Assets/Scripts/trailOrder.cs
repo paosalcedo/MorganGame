28a112c0714c49822f2efb45f9d37328c9b74bbb
Mostafa Haque <93haque@gmail.com>
2016-11-26 19:26:01 -0500

    Put background on Plane. Added Starfall Effect.

---
+++ Assets/Assets/Scripts/trailOrder.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trailOrder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
