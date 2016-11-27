f3870cf6624ea1e54c0959e83bab206df8dd2090
Unknown <paosalcedo@gmail.com>
2016-11-27 03:34:15 -0500

    Added "Success" sound; Only plays when player in contact with correct color

---
+++ Assets/Assets/Scripts/PlaySound.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// THIS IS ATTACHED TO THE EMPTY CHILD OF PLAYER "SuccessSound"
+public class PlaySound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void RightColorSound()
+	{
+		AudioSource sound = GetComponent<AudioSource>();
+		sound.Play ();
+	}
+}
